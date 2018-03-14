d497d449f337152b4c004d5232e8b373dbfe3542
Ethan Strauss <estrauss@gmail.com>
2018-03-14 19:37:59 -0400

    Update script.js
    Working in circles...

--- Simonsays/script.js
+++ Simonsays/script.js
@@ -1,26 +1,31 @@
 let strict = false;
 let humanTurn = false;
-let pattern = [];
+var pattern = [];
+let numDisplay = 0;
 
-function button(name,brightColor,darkColor){
-  this.id = name;
+function button(id,name,brightColor,darkColor,audio){
+  this.id = id;
+  this.name = name;
   this.color = darkColor;
   this.brightColor = brightColor;
+  this.audio = new Audio(audio);
   this.highlight = function () {
     let id = this.id;
     let oldColor = this.color;
-    document.getElementById(id).style.backgroundColor = this.brightColor;
+    console.log(audio);
+    this.audio.play();
+    document.getElementById(name).style.backgroundColor = this.brightColor;
     setTimeout(function() {
-      document.getElementById(id).style.backgroundColor = oldColor;      
+      document.getElementById(name).style.backgroundColor = oldColor;      
     }, 500);
   }
 }
 
 let buttonArray = [
-  new button("upleft","#0000ff", "#0000ba"),
-  new button("upright","#ff0000", "#ba0000"),
-  new button("downleft","#ffff00", "#baba00"),
-  new button("downright","#00ff00", "#00ba00"),
+  new button(0,"upleft","#0000ff", "#0000ba",'https://s3.amazonaws.com/freecodecamp/simonSound1.mp3'),
+  new button(1,"upright","#ff0000", "#ba0000",'https://s3.amazonaws.com/freecodecamp/simonSound2.mp3'),
+  new button(2,"downleft","#ffff00", "#baba00",'https://s3.amazonaws.com/freecodecamp/simonSound3.mp3'),
+  new button(3,"downright","#00ff00", "#00ba00",'https://s3.amazonaws.com/freecodecamp/simonSound4.mp3'),
 ]
 
 let startButton = document.getElementById('startt');
@@ -41,6 +46,8 @@ function strictOn() {
 }
 
 function startGame() {
+  humanTurn = false;
+  pattern = [];
   this.style.backgroundColor = 'red';
   this.style.boxShadow = '0px 0px 3px 3px #400000';
   turn();
@@ -57,25 +64,24 @@ function turn() {
 
 function extendPattern() {
   let newOne = Math.floor(Math.random()*4);
-  pattern.push([buttonArray[newOne].id,newOne]);
-  console.log(pattern);
+  pattern.push([newOne,buttonArray[newOne].name]);
 }
 
 function showPattern() {
   let p=1;
-  pattern.forEach(function(x) {
+  for(let i=0; i<pattern.length; i++) {
     window.setTimeout(function() {
-      let audio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound' + (x[1]+1) + '.mp3');
-      audio.play();
-      buttonArray[x[1]].highlight();
+      console.log(buttonArray[pattern[i][0]]);
+      buttonArray[pattern[i][0]].highlight();
     }, 750*p);
     p++;
-  });
+  }
   humanTurn = true;
   turn();
 }
 
 function human() {
+  let humanPattern = [];  
   let but = document.getElementsByClassName('buttons');
   for(let i=0; i<but.length; i++) {
     but[i].style.cursor = "pointer";
@@ -83,19 +89,18 @@ function human() {
   }
 
   function attempt(e) {
-    let humanPattern = [];
     let nowId = e.target.id;
     humanPattern.push(nowId);
     for(let i=0; i<buttonArray.length; i++) {
-      if (buttonArray[i].id == nowId) {
-        let audio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound' + (i+1) + '.mp3');
-        audio.play();
+      if (buttonArray[i].name == nowId) {
+        console.log(buttonArray[i]);
         buttonArray[i].highlight();
       }
     }
-    humanPattern.forEach((x,ind) => {
-      console.log(x);
-      console.log(ind);
-    });
+    
+    if (humanPattern.length == pattern.length) {
+      humanTurn = false;
+      turn();
+    }
   }
 }
